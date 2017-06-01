c200407511fa7db37defdfd03998a47794a06bcd
Sean <sean8purdue@gmail.com>
2017-06-01 13:47:22 -0400

    fix if else if bug, log history in excel

--- Fundamental/src/BinarySearchSean.java
+++ Fundamental/src/BinarySearchSean.java
@@ -14,7 +14,7 @@ public class BinarySearchSean {
 
             if (key < a[mid]) hi = mid - 1;
 
-            if (key > a[mid]) lo = mid + 1;
+            else if (key > a[mid]) lo = mid + 1;
 
             else return mid;
         }
