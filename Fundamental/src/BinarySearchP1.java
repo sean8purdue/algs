3fa93e65b5e6edae515ac1c5db61e07e05c45ebf
Sean <sean8purdue@gmail.com>
2017-06-02 14:43:44 -0400

    test2 public non-static method used in same class

--- Fundamental/src/BinarySearchP1.java
+++ Fundamental/src/BinarySearchP1.java
@@ -7,9 +7,16 @@ import edu.princeton.cs.algs4.*;
 public class BinarySearchP1 {
 
 //    public static int search (int key, int[] ar) {
+
+
     // test private method;
     // private static also works
-    private static int search (int key, int[] ar) {
+//    private static int search (int key, int[] ar) {
+
+    // test2 public non static method used in the same class
+
+    public int search (int key, int[] ar) {
+
         int lo = 0;
         int hi = ar.length;
         int mid;
@@ -37,15 +44,18 @@ public class BinarySearchP1 {
         for (int i = 0; i < whiteList.length; i++)
             System.out.println(whiteList[i]);
 
+        // test2 public non-static method used in same class
+        BinarySearchP1 bsP1 = new BinarySearchP1();
+
         int key = 6;
-        if(search(key, whiteList) < 0) {
+        if(bsP1.search(key, whiteList) < 0) {
             System.out.format("%d not found", key);
         } else {
             System.out.println("key is found");
         }
 
         key = 7;
-        if(search(key, whiteList) < 0) {
+        if(bsP1.search(key, whiteList) < 0) {
             System.out.format("%d not found", key);
         } else {
             System.out.println("key is found");
