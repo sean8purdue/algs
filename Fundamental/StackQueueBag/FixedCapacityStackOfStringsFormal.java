fd1a17a820b4b68e9e98857731231e208a82cfe2
Sean <sean8purdue@gmail.com>
2017-06-04 14:31:35 -0400

    Add Exception Version

---
+++ Fundamental/StackQueueBag/FixedCapacityStackOfStringsFormal.java
@@ -0,0 +1,56 @@
+/**
+ * Created by Sean on 6/4/17.
+ */
+public class FixedCapacityStackOfStringsFormal {
+    private String[] ar;
+    private int size = 0;  // Next available index in the array
+
+    public FixedCapacityStackOfStringsFormal(int cap) {
+        this.ar = new String[cap];
+    }
+
+    public boolean isEmpty() { return  size == 0; }
+
+    private boolean isFull() {return size == ar.length;}
+
+    public void push(String str) throws Exception {
+
+        if (this.isFull()) {
+            throw new Exception("Stack is Full, can't push");
+        }
+
+        ar[size++] = str;
+    }
+
+    public String pop() throws Exception {
+
+        if (this.isEmpty()) {
+            throw new Exception("Stack is Empty, can't pop");
+        }
+
+        return ar[--size];
+    }
+
+    public static void main(String[] args) {
+        try {
+            FixedCapacityStackOfStringsFormal s = new FixedCapacityStackOfStringsFormal(5);
+            s.push("one");
+            s.push("two");
+            s.push("three");
+            System.out.println(s.pop());
+            System.out.println(s.pop());
+            s.push("four");
+            System.out.println(s.pop());
+            System.out.println(s.pop());
+//            s.pop();  // <-- this causes an underflow exception
+            //     when uncommented
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+}
